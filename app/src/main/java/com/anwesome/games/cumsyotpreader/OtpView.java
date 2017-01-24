fae5f135b01280652aea77e8b0303e965e5d4fdd
Anwesh43 <anweshthecool0@gmail.com>
2017-01-24 16:36:47 +0530

    Adding OtpView

--- app/src/main/java/com/anwesome/games/cumsyotpreader/OtpView.java
+++ app/src/main/java/com/anwesome/games/cumsyotpreader/OtpView.java
@@ -1,10 +1,26 @@
 package com.anwesome.games.cumsyotpreader;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
  * Created by anweshmishra on 24/01/17.
  */
 public class OtpView extends View {
-    public void OtpView
+    public OtpView(Context context) {
+        super(context);
+    }
+    public void onDraw(Canvas canvas) {
+        try {
+
+        }
+        catch (Exception ex) {
+
+        }
+    }
+    public boolean onTouchEvent(MotionEvent event) {
+        return true;
+    }
 }
